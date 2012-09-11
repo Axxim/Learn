06c02900c4064a27f426df09a27b56670002c71f
Luke <luke@axxim.net>
2012-09-11 12:16:10 -0500

    Updated layout

---
+++ application/views/layouts/default.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Bootstrap, from Twitter</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+		<!-- Le styles -->
+		<link href="http://bootstrap.axxim.net/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+		<style type="text/css">
+			body {
+				padding-top: 60px;
+				padding-bottom: 40px;
+				background-color: #FFF;
+			}
+			.sidebar-nav {
+				padding: 9px 0;
+			}
+			#mode {
+				margin-bottom: -18px;
+				margin-left: -1px;
+			}
+			#mode .btn {
+				padding-left: 15px;
+				padding-right: 15px;
+			}
+		</style>
+		<link href="http://bootstrap.axxim.net/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" />
+		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<!-- Le fav and touch icons -->
+		<link rel="shortcut icon" href="../assets/ico/favicon.ico" />
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png" />
+		<link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png" />
+	</head>
+	<body>
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					</a>
+					<a class="brand" href="#">Project name</a>
+					<div class="nav-collapse">
+						<ul class="nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#about">About</a></li>
+							<li><a href="#contact">Contact</a></li>
+						</ul>
+					</div>
+					<!--/.nav-collapse -->
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="span2">
+					<div id="mode" class="btn-group" data-toggle="buttons-radio">
+						<button type="button" class="btn btn-primary">Simple</button>
+						<button type="button" class="btn btn-primary">Advanced</button>
+					</div>
+					<br>
+					<div class="well sidebar-nav">
+						<ul class="nav nav-list">
+							<li class="nav-header">HTML</li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li class="nav-header">CSS</li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li class="nav-header">PHP</li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li class="nav-header">MongoDB</li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+							<li><a href="#">Link</a></li>
+						</ul>
+					</div><!--/.well -->
+				</div>
+				<div class="span10">
+					<!-- Main hero unit for a primary marketing message or call to action -->
+					<div class="hero-unit">
+						<h1>Hello, world!</h1>
+						<p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+						<p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+					</div>
+					<!-- Example row of columns -->
+					<div class="row">
+						<div class="span3">
+							<h2>Heading</h2>
+							<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+							<p><a class="btn" href="#">View details &raquo;</a></p>
+						</div>
+						<div class="span3">
+							<h2>Heading</h2>
+							<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+							<p><a class="btn" href="#">View details &raquo;</a></p>
+						</div>
+						<div class="span3">
+							<h2>Heading</h2>
+							<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+							<p><a class="btn" href="#">View details &raquo;</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<hr />
+			<footer>
+				<p>&copy; Company 2012</p>
+			</footer>
+		</div>
+		<!-- /container -->
+		<!-- Le javascript
+			================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+		<script src="http://bootstrap.axxim.net/bootstrap/js/bootstrap.min.js"></script>
+		<script>
+		/* Autogenerate from database */
+		var simple = {
+			'HTML': {
+				'/html/': 'The Basics', // What is HTML, history behind it, etc
+				'/html/good-vs-bad': 'Good Vs. Bad' // Learn good vs bad HTML
+			},
+			'CSS': {
+				
+			}
+		};
+		</script>
+	</body>
+</html>
